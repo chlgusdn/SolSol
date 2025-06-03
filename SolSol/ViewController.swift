d443ac7dc058fcbe7a53b7e042bfc3705ed4c5cc
NUNU:D <hwchoi@caremedi.co.kr>
2025-06-03 14:38:47 +0900

    Initial Commit

---
+++ SolSol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolSol
+//
+//  Created by NUNU:D on 6/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SolSolTests/SolSolTests.swift
@@ -0,0 +1,17 @@
+//
+//  SolSolTests.swift
+//  SolSolTests
+//
+//  Created by NUNU:D on 6/3/25.
+//
+
+import Testing
+@testable import SolSol
+
+struct SolSolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

52d39f308d4cedcb3d5b17424344e2073dca2d98
NUNU:D <hynjni7890@gmail.com>
2026-04-09 00:47:54 +0900

    [fix]: SwiftUI기반 extension 제거

--- Projects/DesignSystem/Sources/Extensions/UIView+Extension.swift
+++ Projects/DesignSystem/Sources/Extensions/UIView+Extension.swift
@@ -5,14 +5,6 @@
 //  Created by NUNU:D on 10/5/25.
 //
 
-import SwiftUI
+import UIKit
 
-public extension View {
-    
-    func toUIKitView(for viewController: UIViewController) -> UIView {
-        let hostingController = UIHostingController(rootView: self)
-        hostingController.view.backgroundColor = .clear
-        viewController.addChild(hostingController)
-        return hostingController.view
-    }
-}
+public extension UIView {}
